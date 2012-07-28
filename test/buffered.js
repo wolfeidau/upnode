4da020438c8f4aa51d63b10be55a2b26cabb91db
James Halliday <mail@substack.net>
2012-07-27 18:20:41 -0700

    buffered test fixed

--- test/buffered.js
+++ test/buffered.js
@@ -3,7 +3,7 @@ var dnode = require('dnode');
 var test = require('tap').test;
 var net = require('net');
 
-test('simple', function (t) {
+test('buffered connections', function (t) {
     t.plan(5);
     var port = Math.floor(Math.random() * 5e4 + 1e4);
     var up = upnode.connect(port);
@@ -12,9 +12,7 @@ test('simple', function (t) {
     for (var i = 0; i < 5; i++) {
         setTimeout(function () {
             up(function (remote) {
-console.log('up'); 
                 remote.time(function (time) {
-console.log('time=' + time); 
                     t.ok(time);
                 });
             });
@@ -31,14 +29,11 @@ console.log('time=' + time);
             this.ping = function (cb) { cb() };
         });
         ds.pipe(stream).pipe(ds);
-        
-        t.on('end', function () {
-            ds.end();
-        });
     });
     server.listen(port);
     
     t.on('end', function () {
         server.close();
+        up.close();
     });
 });
